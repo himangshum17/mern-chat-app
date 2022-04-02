e09e6cf785a4ff3c6f0cbcad198963a24196e00d
Himangshu Mondal <mailtoherock@gmail.com>
2022-04-02 06:04:32 +0530

    add react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import Chatpage from './pages/chatpage';
+import Homepage from './pages/homepage';
+
 function App() {
   return (
-    <h1 className='text-3xl font-bold text-green-600 underline'>
-      Hello world!
-    </h1>
+    <>
+      <h1 className='text-3xl font-bold text-green-600 underline'>
+        Hello world!
+      </h1>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='chat' element={<Chatpage />} />
+      </Routes>
+    </>
   );
 }
 
