ade53de39ef7dba164167b3ec76baa1700600009
Himangshu Mondal <mailtoherock@gmail.com>
2022-04-05 23:21:20 +0530

    connect mongoDB

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
-cors = require('cors');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+dotenv.config();
 const app = express();
 app.use(cors());
-const port = 5000;
+const port = process.env.PORT;
+
+connectDB();
 
 const users = [
   { id: 1, name: 'Leanne Graham' },
