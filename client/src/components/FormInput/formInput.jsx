259ae10519d6ef3f88d57b02c4bd1730d5b2a23a
Himangshu Mondal <mailtoherock@gmail.com>
2022-04-09 00:50:13 +0530

    add login form validation

--- client/src/components/FormInput/formInput.jsx
+++ client/src/components/FormInput/formInput.jsx
@@ -1,18 +1,25 @@
-const FormInput = ({ label, ...otherFormInputProps }) => {
+const FormInput = ({ register, error, ...otherFormInputProps }) => {
+  // console.log(otherFormInputProps);
   return (
     <div>
-      {label && (
+      {otherFormInputProps.label && (
         <label
           className='text-sm leading-7 text-gray-600'
           htmlFor={otherFormInputProps.id}
         >
-          {label}
+          {otherFormInputProps.label}
         </label>
       )}
       <input
         {...otherFormInputProps}
+        {...register(`${otherFormInputProps.name}`)}
         className='w-full rounded border border-gray-300 bg-white py-1 px-3 text-sm leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out  focus:ring-2 focus:ring-indigo-500'
       />
+      {error && (
+        <span className='mt-1 block text-sm text-red-500' role='alert'>
+          {error}
+        </span>
+      )}
     </div>
   );
 };
--- client/src/pages/loginpage.jsx
+++ client/src/pages/loginpage.jsx
@@ -1,52 +1,62 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import { Button, FormInput } from '../components';
-const initialFormData = {
-  email: '',
-  password: '',
-};
-const Loginpage = () => {
-  const [loginFormData, setLoginFormData] = useState(initialFormData);
-  const handleInputChange = e => {
-    const { name, value } = e.target;
-    setLoginFormData({ ...loginFormData, [name]: value });
-  };
-  const handleFormSubmit = async e => {
-    e.preventDefault();
+const validationSchema = yup
+  .object({
+    email: yup
+      .string()
+      .required('Email Address is required')
+      .email('Email Address is not valid'),
+    password: yup.string().required('Password is required'),
+  })
+  .required();
 
+const Loginpage = () => {
+  const formOptions = { resolver: yupResolver(validationSchema) };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting, isSubmitSuccessful },
+    reset,
+  } = useForm(formOptions);
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+    }
+  }, [isSubmitSuccessful, reset]);
+  const onSubmit = async formData => {
     const config = {
       headers: {
         'Content-Type': 'application/json',
       },
     };
-
-    console.log(loginFormData);
-
     try {
       const { data } = await axios.post(
         `${import.meta.env.VITE_BASE_URL}/api/user/login`,
-        loginFormData,
+        formData,
         config
       );
     } catch (error) {
       console.log(error.message);
     }
-    setLoginFormData(initialFormData);
   };
   return (
     <section className='flex h-screen w-full items-center justify-center bg-gray-800 py-16'>
       <div className='w-full max-w-xl rounded-md bg-gray-100 px-6 py-8'>
         <h1 className='text-2xl'>Lets Connect</h1>
-        <form className='mt-6 grid gap-4' onSubmit={handleFormSubmit}>
+        <form className='mt-6 grid gap-4' onSubmit={handleSubmit(onSubmit)}>
           <FormInput
             label='Email address'
             type='email'
             id='email'
             name='email'
             placeholder='Enter your Email address'
-            value={loginFormData.email}
-            onChange={handleInputChange}
+            register={register}
+            error={errors.email?.message}
           />
           <FormInput
             label='Password'
@@ -54,11 +64,43 @@ const Loginpage = () => {
             id='password'
             name='password'
             placeholder='Enter your Password'
-            value={loginFormData.password}
-            onChange={handleInputChange}
+            register={register}
+            error={errors.password?.message}
           />
-          <Button extraclassName='justify-self-start' type='submit'>
-            Login
+          <Button
+            disabled={isSubmitting}
+            extraClasses={`justify-self-start ${
+              isSubmitting ? 'cursor-not-allowed' : ''
+            }`}
+            type='submit'
+          >
+            {isSubmitting ? (
+              <>
+                <svg
+                  className='-ml-1 mr-3 h-5 w-5 animate-spin text-gray-700'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                >
+                  <circle
+                    className='opacity-25'
+                    cx='12'
+                    cy='12'
+                    r='10'
+                    stroke='currentColor'
+                    strokeWidth='4'
+                  ></circle>
+                  <path
+                    className='opacity-75'
+                    fill='currentColor'
+                    d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+                  ></path>
+                </svg>{' '}
+                Processing...
+              </>
+            ) : (
+              'Login'
+            )}
           </Button>
         </form>
         <p className='mt-6 text-base text-gray-600'>
