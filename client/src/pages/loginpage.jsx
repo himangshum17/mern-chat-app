5a80cfe37c2f9b21aff65f6809aea1190ba5833e
Himangshu Mondal <mailtoherock@gmail.com>
2022-04-08 23:58:19 +0530

    create user login

--- client/src/pages/loginpage.jsx
+++ client/src/pages/loginpage.jsx
@@ -1,18 +1,52 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, FormInput } from '../components';
-
+const initialFormData = {
+  email: '',
+  password: '',
+};
 const Loginpage = () => {
+  const [loginFormData, setLoginFormData] = useState(initialFormData);
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+    setLoginFormData({ ...loginFormData, [name]: value });
+  };
+  const handleFormSubmit = async e => {
+    e.preventDefault();
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    console.log(loginFormData);
+
+    try {
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_BASE_URL}/api/user/login`,
+        loginFormData,
+        config
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+    setLoginFormData(initialFormData);
+  };
   return (
     <section className='flex h-screen w-full items-center justify-center bg-gray-800 py-16'>
       <div className='w-full max-w-xl rounded-md bg-gray-100 px-6 py-8'>
         <h1 className='text-2xl'>Lets Connect</h1>
-        <form className='mt-6 grid gap-4'>
+        <form className='mt-6 grid gap-4' onSubmit={handleFormSubmit}>
           <FormInput
             label='Email address'
             type='email'
             id='email'
             name='email'
             placeholder='Enter your Email address'
+            value={loginFormData.email}
+            onChange={handleInputChange}
           />
           <FormInput
             label='Password'
@@ -20,6 +54,8 @@ const Loginpage = () => {
             id='password'
             name='password'
             placeholder='Enter your Password'
+            value={loginFormData.password}
+            onChange={handleInputChange}
           />
           <Button extraclassName='justify-self-start' type='submit'>
             Login
