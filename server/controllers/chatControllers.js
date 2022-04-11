d80efeecc83cdd4b2f229bc21d818ab31fbe3fb4
Himangshu Mondal <mailtoherock@gmail.com>
2022-04-11 21:55:39 +0530

    create remove from group api

--- server/controllers/chatControllers.js
+++ server/controllers/chatControllers.js
@@ -144,10 +144,34 @@ const addToGroup = asyncHandler(async (req, res) => {
   }
 });
 
+const removeFromGroup = asyncHandler(async (req, res) => {
+  const { chatId, userId } = req.body;
+
+  const removed = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $pull: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate('users', '-password')
+    .populate('groupAdmin', '-password');
+
+  if (!removed) {
+    res.status(404);
+    throw new Error('Chat Not Found');
+  } else {
+    res.json(removed);
+  }
+});
+
 module.exports = {
   accessChat,
   fetchChats,
   createGroupChat,
   renameGroup,
   addToGroup,
+  removeFromGroup,
 };
--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -5,6 +5,7 @@ const {
   createGroupChat,
   renameGroup,
   addToGroup,
+  removeFromGroup,
 } = require('../controllers/chatControllers');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
@@ -14,5 +15,6 @@ router.route('/').get(protect, fetchChats);
 router.route('/group').post(protect, createGroupChat);
 router.route('/rename').put(protect, renameGroup);
 router.route('/groupadd').put(protect, addToGroup);
+router.route('/groupremove').put(protect, removeFromGroup);
 
 module.exports = router;
