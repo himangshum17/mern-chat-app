e2d8748fbc3ad51203f7a16e8539949c58ebc6ce
Himangshu Mondal <mailtoherock@gmail.com>
2022-04-11 21:52:24 +0530

    create add to group api

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -4,6 +4,7 @@ const {
   fetchChats,
   createGroupChat,
   renameGroup,
+  addToGroup,
 } = require('../controllers/chatControllers');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
@@ -12,5 +13,6 @@ router.route('/').post(protect, accessChat);
 router.route('/').get(protect, fetchChats);
 router.route('/group').post(protect, createGroupChat);
 router.route('/rename').put(protect, renameGroup);
+router.route('/groupadd').put(protect, addToGroup);
 
 module.exports = router;
