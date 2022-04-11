d4964cde34358327181c5a9c5a531a60bfbe9c05
Himangshu Mondal <mailtoherock@gmail.com>
2022-04-11 21:36:35 +0530

    create group chat api

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const { accessChat, fetchChats } = require('../controllers/chatControllers');
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+} = require('../controllers/chatControllers');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.route('/').post(protect, accessChat);
 router.route('/').get(protect, fetchChats);
+router.route('/group').post(protect, createGroupChat);
 
 module.exports = router;
